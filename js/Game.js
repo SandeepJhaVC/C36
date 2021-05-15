5cc51d52b6e1ff7e3694cdda3c0fa95d616d64e6
sandeepjhaVC <heyyoumailmeat@gmail.com>
2021-05-15 18:00:12 +0530

    playState

--- js/Game.js
+++ js/Game.js
@@ -1,26 +1,54 @@
 class Game {
-  constructor(){}
-  
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
+  constructor() { }
+
+  getState() {
+    var gameStateRef = database.ref('gameState');
+    gameStateRef.on("value", (data) => {
+      gameState = data.val();
     })
-   
+
   }
 
-  update(state){
+  update(state) {
     database.ref('/').update({
       gameState: state
     });
   }
 
-  start(){
-    if(gameState === 0){
+  async start() {
+    if (gameState === 0) {
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if (playerCountRef.exists()) {
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
       form = new Form()
       form.display();
+    }
+  }
+
+  play() {
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100);
+    Player.getPlayerInfo();
+    if (allPlayers !== undefined) {
+      var dp = 130;
+      for (var plr in allPlayers) {
+        if (plr === "player" + player.index) {
+          fill("red");
+        } else {
+          fill("black");
+        }
+        dp += 20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120, dp);
+      }
+    }
+    if (keyIsDown(UP_ARROW) && player.index !== null) {
+      player.distance += 50;
+      player.update();
     }
   }
 }
